b45e40c7c70ac6dcf948ade6c3f2649ea7c671f7
CSimpson-Cannella <140020817+CSimpson-Cannella@users.noreply.github.com>
2023-07-19 23:27:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create a web server
+// Path: server.js
+// Express is a web server framework for Node.js
+// Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+// We will use it to create a web server
+// Express is the most popular web server framework for Node.js
+// To install Express, run the following command at the terminal:
+// npm install express
+// Create a file called server.js
